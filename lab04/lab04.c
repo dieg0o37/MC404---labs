22b4d7612a157ce402b5fa2505dba00ef3f7ac35
DIEGO <d288809@dac.unicamp.br>
2025-04-03 14:40:05 -0300

    final

--- lab04/lab04.c
+++ lab04/lab04.c
@@ -1,47 +1,46 @@
 
-// int read(int __fd, const void *__buf, int __n) {
-//     int ret_val;
-//     __asm__ __volatile__(
-//         "mv a0, %1    # file descriptor\n"
-//         "mv a1, %2    # buffer \n"
-//         "mv a2, %3    # size \n"
-//         "li a7, 63    # syscall read (63) \n"
-//         "ecall        # invoke syscall \n"
-//         "mv %0, a0    # move return value to ret_val\n"
-//         : "=r"(ret_val) // Output list
-//         : "r"(__fd), "r"(__buf), "r"(__n) // Input list
-//         : "a0", "a1", "a2", "a7"
-//     );
-//     return ret_val;
-// }
+int read(int __fd, const void *__buf, int __n) {
+    int ret_val;
+    __asm__ __volatile__(
+        "mv a0, %1    # file descriptor\n"
+        "mv a1, %2    # buffer \n"
+        "mv a2, %3    # size \n"
+        "li a7, 63    # syscall read (63) \n"
+        "ecall        # invoke syscall \n"
+        "mv %0, a0    # move return value to ret_val\n"
+        : "=r"(ret_val) // Output list
+        : "r"(__fd), "r"(__buf), "r"(__n) // Input list
+        : "a0", "a1", "a2", "a7"
+    );
+    return ret_val;
+}
 
-// void write(int __fd, const void *__buf, int __n) {
-//     __asm__ __volatile__(
-//         "mv a0, %0    # file descriptor\n"
-//         "mv a1, %1    # buffer \n"
-//         "mv a2, %2    # size \n"
-//         "li a7, 64    # syscall write (64) \n"
-//         "ecall"
-//         : // Output list
-//         : "r"(__fd), "r"(__buf), "r"(__n)   // Input list
-//         : "a0", "a1", "a2", "a7"
-//     ); 
-// }
+void write(int __fd, const void *__buf, int __n) {
+    __asm__ __volatile__(
+        "mv a0, %0    # file descriptor\n"
+        "mv a1, %1    # buffer \n"
+        "mv a2, %2    # size \n"
+        "li a7, 64    # syscall write (64) \n"
+        "ecall"
+        : // Output list
+        : "r"(__fd), "r"(__buf), "r"(__n)   // Input list
+        : "a0", "a1", "a2", "a7"
+    ); 
+}
 
-// void exit(int code) {
-//     __asm__ __volatile__(
-//         "mv a0, %0    # return code\n"
-//         "li a7, 93    # syscall exit (93) \n"
-//         "ecall"
-//         : // Output list
-//         : "r"(code)    // Input list
-//         : "a0", "a7"
-//     );
-// }
+void exit(int code) {
+    __asm__ __volatile__(
+        "mv a0, %0    # return code\n"
+        "li a7, 93    # syscall exit (93) \n"
+        "ecall"
+        : // Output list
+        : "r"(code)    // Input list
+        : "a0", "a7"
+    );
+}
 #define STDIN_FD 0
 #define STDOUT_FD 1
 #define INPUT_SIZE 48
-#include <stdio.h>
 
 
 void bin_to_hex (char *num, char *num_hex) {
@@ -65,7 +64,7 @@ void bin_to_hex (char *num, char *num_hex) {
         if (hex_digit < 10) {
             num_hex[len++] = hex_digit + '0';
         } else {
-            num_hex[len++] = hex_digit - 10 + 'a';
+            num_hex[len++] = hex_digit - 10 + 'A';
         }
     }
 
@@ -75,17 +74,18 @@ void bin_to_hex (char *num, char *num_hex) {
         num_hex[i] = num_hex[len - i - 1];
         num_hex[len - i - 1] = temp;
     }
-    num_hex[len] = '\0';
+    num_hex[len] = '\n';
 }
 
 
 void strdec_to_strbin(char num[5], char num_bin[33]) {
+    //converte o número decimal para binário
     int sign = num[0] == '-' ? -1 : 1;
     int abs_value = (num[1] - '0') * 1000 + (num[2] - '0') * 100 + (num[3] - '0') * 10 + (num[4] - '0');
-    int value = sign * abs_value;
-    unsigned int u = (unsigned int)value;
+    int value = sign * abs_value; // considera o sinal
+    unsigned int u = (unsigned int)value; // salva o numero no formato unsigned int
     for (int i = 0; i < 32; i++) {
-        unsigned int bit = (u >> (31 - i)) & 1;
+        unsigned int bit = (u >> (31 - i)) & 1; // extrai o bit i
         num_bin[i] = bit ? '1' : '0';
     }
     num_bin[32] = '\0';
@@ -93,9 +93,9 @@ void strdec_to_strbin(char num[5], char num_bin[33]) {
 
 void read_to_bin(char n1_bin[], char n2_bin[], char n3_bin[], char n4_bin[], char n5_bin[], char n6_bin[], char n7_bin[], char n8_bin[]){
     //le os números decimais
-    char input[INPUT_SIZE] = "-0001 -0001 -0001 -0001 -0001 -0001 -0001 +0000\n";
-    // read(STDIN_FD, input, INPUT_SIZE);
-    //8 numbers
+    char input[INPUT_SIZE];
+    read(STDIN_FD, input, INPUT_SIZE);
+    //8 numeros
     char n1_dec[5], n2_dec[5], n3_dec[5], n4_dec[5], n5_dec[5], n6_dec[5], n7_dec[5], n8_dec[5];
     int i = 0, j = 0;
     for (i = 0; i < 5; i++){
@@ -124,6 +124,8 @@ void read_to_bin(char n1_bin[], char n2_bin[], char n3_bin[], char n4_bin[], cha
 
 void xor(char n1_bin[], char n2_bin[], char result_bin[]){
     for (int i = 0; i < 32; i++){
+        // se os bits forem diferentes, o resultado é 1
+        // se forem iguais, o resultado é 0
         if (n1_bin[i] == n2_bin[i]){
             result_bin[i] = '0';
         } else {
@@ -135,6 +137,8 @@ void xor(char n1_bin[], char n2_bin[], char result_bin[]){
 
 void and(char n1_bin[], char n2_bin[], char result_bin[]){
     for (int i = 0; i < 32; i++){
+        // se ambos os bits forem 1, o resultado é 1
+        // se um deles for 0, o resultado é 0
         if (n1_bin[i] == '1' && n2_bin[i] == '1'){
             result_bin[i] = '1';
         } else {
@@ -146,6 +150,8 @@ void and(char n1_bin[], char n2_bin[], char result_bin[]){
 
 void nand(char n1_bin[], char n2_bin[], char result_bin[]){
     for (int i = 0; i < 32; i++){
+        // se ambos os bits forem 1, o resultado é 0
+        // se um deles for 0, o resultado é 1
         if (n1_bin[i] == '1' && n2_bin[i] == '1'){
             result_bin[i] = '0';
         } else {
@@ -157,6 +163,8 @@ void nand(char n1_bin[], char n2_bin[], char result_bin[]){
 
 void or(char n1_bin[], char n2_bin[], char result_bin[]){
     for (int i = 0; i < 32; i++){
+        // se um dos bits for 1, o resultado é 1
+        // se ambos forem 0, o resultado é 0
         if (n1_bin[i] == '1' || n2_bin[i] == '1'){
             result_bin[i] = '1';
         } else {
@@ -170,15 +178,6 @@ int main(){
     char n1_bin[33], n2_bin[33], n3_bin[33], n4_bin[33], n5_bin[33], n6_bin[33], n7_bin[33], n8_bin[33];
 
     read_to_bin(n1_bin, n2_bin, n3_bin, n4_bin, n5_bin, n6_bin, n7_bin, n8_bin);
-    //imprime os números binários
-    printf("n1_bin: %s\n", n1_bin);
-    printf("n2_bin: %s\n", n2_bin);
-    printf("n3_bin: %s\n", n3_bin);
-    printf("n4_bin: %s\n", n4_bin);
-    printf("n5_bin: %s\n", n5_bin);
-    printf("n6_bin: %s\n", n6_bin);
-    printf("n7_bin: %s\n", n7_bin);
-    printf("n8_bin: %s\n", n8_bin);
 
     char N1[33], N2[33], N3[33], N4[33];
     and(n1_bin, n2_bin, N1);
@@ -197,13 +196,14 @@ int main(){
     val_final_str[32] = '\0';
     char val_final_hex[33];
     bin_to_hex(val_final_str, val_final_hex);
-    printf("val_final_str: 0x%s\n", val_final_hex);
+    write(STDOUT_FD, "0x", 2);
+    write(STDOUT_FD, val_final_hex, 9);
     
     return 0;
 }
 
-// void _start()
-// {
-//   int ret_code = main();
-//   exit(ret_code);
-// }
+void _start()
+{
+  int ret_code = main();
+  exit(ret_code);
+}
